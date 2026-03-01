db270d9763437aa56ef19e250762b18f7e2cc471
Jetsadakorn Saelee <jetsadakorn.sae@student.mahidol.ac.th>
2026-03-01 15:40:02 +0700

    Add Notification, NotificationService

--- NotificationService.java
+++ NotificationService.java
@@ -1,3 +1,8 @@
 public interface NotificationService {
-    void sendNotification(String message);
+    Notification getNotification();
+    void sendNotification();
+    void pushNotification();
+    void pushNotification(String msg, String title);
+    void markAsRead(Notification n);
+    void clearAllNotification();
 }
